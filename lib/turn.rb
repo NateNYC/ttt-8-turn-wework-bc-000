1fa6bdc9be65f4f70d5d92947cb703cb2c2cd998
Nathan de Paz <nathan.depaz@wework.com>
2018-02-15 20:34:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,9 @@
 
 def turn(board)
   
-  index = " "
+  puts "Please enter 1-9: "
+  input=gets.strip
+  index = input_to_index(input)
   
   until valid_move?(board, index)
    puts "Please enter 1-9:"
